32222ca0e77bc4af147f2c1685a9ee1f84a5780c
JohnCarrera <johnrcarrera@gmail.com>
2022-07-06 16:57:40 -0400

    dynamic array added

---
+++ week5-day3-dynamic-array-long-practice/dynamic-array.js
@@ -0,0 +1,61 @@
+class DynamicArray {
+
+  constructor(defaultSize=4) {
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(this.capacity);
+  }
+
+  read(index) {
+    return this.data[index];
+  }
+
+  push(val) {
+    if(this.length === this.capacity){
+      this.resize();
+    }
+    this.data[this.length] = val;
+    this.length++;
+  }
+
+
+  pop() {
+    let pop = this.data[this.length - 1];
+    this.data[this.length - 1] = undefined;
+    this.length--;
+    return pop;
+  }
+
+  shift() {
+    let shift = this.data[0];
+    for(let idx = 0; idx < this.length; idx++) {
+      this.data[idx] = this.data[idx + 1];
+    }
+    this.data[this.length - 1] = undefined;
+    this.length--;
+    return shift;
+  }
+
+  unshift(val) {
+    if(this.length === this.capacity){
+      this.resize();
+    }
+    for(let idx = this.length - 1; idx >= 0; idx--) {
+      this.data[idx+1] = this.data[idx];
+    }
+    this.data[0] = val;
+    this.length++;
+  }
+
+  indexOf (val) {
+
+  }
+
+  resize () {
+
+  }
+
+}
+
+
+module.exports = DynamicArray;
